d2ce116d378de900bf044071dda857cae8b1816d
Saulo Henrique Alves Deles <saulohenriquealvesdeles@MacBook-Air-de-Saulo.local>
2025-09-21 00:14:25 -0300

    Fix: Corrige estrutura do repositório e adiciona arquivos do projeto2

---
+++ painel-web/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,29 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api85e3a35d": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "painelwebdcac5815": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "painelweb05e5b4bf": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
